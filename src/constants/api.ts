03bb421f011c78eb0d0b7236b9035b437e70a7bd
Shadi Abdelsalam <floydanthebeast@gmail.com>
2023-04-01 01:05:14 +0300

    chore: Add env variables support

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,7 +2,7 @@ import OpenAPIClientAxios from 'openapi-client-axios';
 
 import { Client } from 'src/schema';
 
-export const BASE_API_URL = 'http://158.160.37.250';
+export const BASE_API_URL = process.env.API_URL || 'http://158.160.37.250';
 export const ACCESS_TOKEN_LOCAL_STORAGE_KEY = 'accessToken';
 
 export enum UserRole {
@@ -14,7 +14,7 @@ const api = new OpenAPIClientAxios({
   axiosConfigDefaults: {
     baseURL: BASE_API_URL,
   },
-  definition: 'http://158.160.37.250/swagger/v1/swagger.json',
+  definition: `${BASE_API_URL}/swagger/v1/swagger.json`,
 });
 api.init();
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 
+import { config as dotenvConfig } from 'dotenv';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
+
+dotenvConfig();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
